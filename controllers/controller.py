51537aa7e4914f5e88fd1526c8600c15fd5937f6
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-11-06 20:47:46 +0000

    MVP complete

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
 from models.book_list import books
 from models.book import Book
+from models.book_list import add_new_book, remove_book
 
 
 
@@ -15,3 +16,17 @@ def index():
 def single_book(index):
     selected_book = books[int(index)]
     return render_template('book.html', book=selected_book)
+
+@app.route('/library', methods=['POST'])
+def add_book():
+    book_title = request.form['title']
+    book_author = request.form['author']
+    book_genre = request.form['genre']
+    new_book = Book(book_title, book_author, book_genre)
+    add_new_book(new_book)
+    return render_template('index.html', title='Home', books=books)
+
+@app.route('/library/delete/<title>', methods=['POST'])
+def delete(title):
+  remove_book(title)
+  return redirect('/library')
--- models/book_list.py
+++ models/book_list.py
@@ -5,3 +5,15 @@ book2 = Book("The Silmarillion", "J. R. R. Tolkien", "Fantasy Fiction")
 book3 = Book("Daft Wee Stories", "Limmy", "Humour")
 
 books = [book1, book2, book3]
+
+def add_new_book(book):
+    books.append(book)
+
+def remove_book(book_title):
+    book_to_remove = None
+    for book in books:
+        if book.title == book_title:
+            book_to_remove = book
+            break
+
+    books.remove(book_to_remove)
