d49f8df62929318c20c59d6f302f1fa643b85ea1
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-11-06 21:47:17 +0000

    slight css update still terrible

--- models/book_list.py
+++ models/book_list.py
@@ -3,8 +3,9 @@ from models.book import *
 book1 = Book("Who Moved My Cheese?", "Dr Spencer Johnson", "Self-help")
 book2 = Book("The Silmarillion", "J. R. R. Tolkien", "Fantasy Fiction")
 book3 = Book("Daft Wee Stories", "Limmy", "Humour")
+book4 = Book("My Booky Wook", "Russell Brand", "Utter Pish")
 
-books = [book1, book2, book3]
+books = [book1, book2, book3, book4]
 
 def add_new_book(book):
     books.append(book)
